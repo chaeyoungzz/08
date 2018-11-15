0460e3b4634792aae7861ebb8ad897fd41afca4b
chaeyoung <456codud@naver.com>
2018-11-15 16:05:48 +0900

    함수 포인터
    + - * /

--- main.c
+++ main.c
@@ -2,16 +2,52 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char *proverb = "All that glisters is not gold.";
-void setPointer(char **q)
+int sumf(int a, int b)
 {
-	*q = proverb;
+	return a+b;
 }
+int subf(int a, int b)
+{
+	return a-b;
+}
+int mulf(int a, int b)
+{
+	return a*b;
+}
+int divf(int a, int b)
+{
+	return a/b;
+}
+
+
+
 int main(int argc, char *argv[]) {
+	int x,y;
+	char C;
+	int (*pf)(int,int);
 	
-	char *p = "zzz";
-	setPointer(&p);
-	printf("%S\n", p);
+	printf(" input calculation : ");
+	scanf("%i %s %i", &x, &C, &y);
 	
+	switch(C)
+	{
+		case '+':
+		pf=sumf;
+		break;
+		
+		case '-':
+		pf=subf;
+		break;
+		
+		case '*':
+		pf=mulf;
+		break;
+		
+		case '/':
+		pf=divf;
+		break;
+	}
+
+	printf(" output : %i", pf(x,y));
 	return 0;
 }
